bf02358931964c89433dade07585db11fe51194c
Frank Carver <frank@efsol.com>
2018-10-10 09:39:57 +0100

    added an example of assertEquals

--- src/test/java/test/SimpleTest.java
+++ src/test/java/test/SimpleTest.java
@@ -1,6 +1,7 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,9 +10,14 @@ import uk.ac.uos.i2p.SimpleForGit;
 class SimpleTest {
 
 	@Test
+	void testBasicMaths() {
+		assertEquals(4, 2+2, "two plus two should be 4");
+	}
+
+	@Test
 	void testNameNotNull() {
 		SimpleForGit sg = new SimpleForGit();
-		assertNotNull(sg.getName());
+		assertNotNull(sg.getName(), "Even a fresh SimpleForGit object should have a name");
 	}
 
 }
